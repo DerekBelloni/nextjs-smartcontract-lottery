73275d7fa1a1d6218f897138b13dd655de2d01f4
Derek <derekbelloni10@gmail.com>
2023-01-29 12:41:34 -0700

    brought in moralis and tailwind

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -1,5 +1,16 @@
-import {useMoralis} from "react-moralis";
+import { useMoralis } from "react-moralis";
+import { useEffect } from "react";
 
 export default function Header() {
-  return(<div>Hi from Header</div>)
+  const {enableWeb3, account} = useMoralis();
+
+  useEffect(() => {}, [])
+
+  return(<div className="mx-4 my-4">
+    {account ? 
+  (<div className="text-slate-500 font-semibold">Connected to {account.slice(0,6)}...{account.slice(account.length - 4)}</div>) : 
+    (<button className="rounded bg-slate-400 shadow px-2" onClick={async () => {
+      await enableWeb3()
+    }}>Connect</button>)}
+  </div>)
 }
