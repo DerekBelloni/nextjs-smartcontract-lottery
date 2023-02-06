6965edc8190be678363c67f78e6da682547c1d99
Derek <derekbelloni10@gmail.com>
2023-02-05 20:34:13 -0700

    deploying site through ipfs

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,11 @@
 import {ConnectButton} from "web3uikit";
 
 export default function Header() {
-  return (<div className="mt-4">
-    <span className="text-slate-500 underline font-semibold ml-6 mb-2">Decentralized Lottery!</span>
-    <ConnectButton moralisAuth={false}/>
+  return (
+  <div className="mt-4 flex flex-row justify-between">
+    <span className="text-slate-500 underline font-semibold ml-6 text-xl">Decentralized Lottery!</span>
+    <div className="">
+      <ConnectButton moralisAuth={false}/>
+    </div>
   </div>)
 }
--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -3,14 +3,19 @@ import  { abi, contractAddresses } from "../constants";
 import { useMoralis, useWeb3Contract } from "react-moralis";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
 
 export default function LotteryEntrance() {
   const { Moralis, isWeb3Enabled, chainId: chainIdHex} = useMoralis();
   const chainId = parseInt(chainIdHex);
   const lotteryAddress = chainId in contractAddresses?  contractAddresses[chainId][0] : null;
   const [entranceFee, setEntranceFee] = useState("0");
+  const [numPlayers, setNumPlayers] = useState("0");
+  const [recentWinner, setRecentWinner] = useState("0");
 
-  const {runContractFunction: enterLottery} = useWeb3Contract({
+  const dispatch = useNotification();
+
+  const {runContractFunction: enterLottery, isLoading, isFetching} = useWeb3Contract({
     abi: abi,
     contractAddress: lotteryAddress, // specify networkId
     functionName:"enterLottery",
@@ -25,26 +30,83 @@ export default function LotteryEntrance() {
     params: {}
   })
 
+  const {runContractFunction: getNumberofPlayers} = useWeb3Contract({
+    abi: abi,
+    contractAddress: lotteryAddress, // specify networkId
+    functionName:"getNumberofPlayers",
+    params: {}
+  })
+
+  const {runContractFunction: getRecentWinner} = useWeb3Contract({
+    abi: abi,
+    contractAddress: lotteryAddress, // specify networkId
+    functionName:"getRecentWinner",
+    params: {}
+  })
+
+  async function updateUI() {
+    const entranceFeeFromCall = (await getEntranceFee())?.toString();
+    setEntranceFee(entranceFeeFromCall);
+  }
+
   useEffect(() => {
     if (isWeb3Enabled) {
       // try to read lottery entrance fee
     //  msgValue = getEntranceFee();
-    async function updateUI() {
-      const entranceFeeFromCall = (await getEntranceFee()).toString();
-      setEntranceFee(entranceFeeFromCall);
-    }
     updateUI();
 
     }
   }, [isWeb3Enabled])
+
+  const handleSuccess = async (tx) => {
+    await tx.wait(1);
+    handleNewNotification(tx);
+    updateUI();
+  }
+
+  const handleNewNotification = () => {
+    dispatch({
+      type: "info",
+      message: "Transaction Complete!",
+      title: "Transaction Notification",
+      position: "topR",
+      icon: "bell"
+    })
+  }
+
   return (
     <>
+    <div className="grid grid-cols-10">
+    <div className="shadow bg-white rounded mx-4 col-span-6 py-4">
     { lotteryAddress ? 
-    <div className="text-slate-500 font font-semibold mx-6 mt-4 flex flex-col">
+    <div className="text-slate-500 font-semibold mx-6  flex flex-col">
       Lottery entrance fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH
-      <button className="bg-red-500 rounded shadow text-white mt-4" onClick={enterLottery}>Enter Lottery</button>
+      <button className="bg-blue-500 hover:bg-blue-700 rounded shadow text-white mt-4 px-2 " onClick={async function() {
+        await enterLottery({
+          onSuccess: handleSuccess,
+          onError: (error) => console.log(error)
+        })
+        }}
+        disabled={isLoading || isFetching}
+        >{isLoading || isFetching ? <div className=" animate-spin spinner-border h-5 w-5 border-b-2 rounded-full"></div> : <div>Enter Lottery!</div>}</button>
     </div> : 
-    <div>No Lottery Address Detected</div>}
+    <div className="text-slate-500 font-semibold mx-6">No Lottery Address Detected</div>}
+    <div className="">
+      <button className="bg-green-500 hover:bg-green-700 mx-6 rounded shadow text-white mt-4" onClick={async () => {
+        const numPlayersFromCall = (await getNumberofPlayers())?.toString();
+        setNumPlayers(numPlayersFromCall);
+      }}>Number of Players</button>
+      <span className="text-slate-500 font-semibold">There are {numPlayers} players entered into the lottery!</span>
+    </div>
+    <div className="">
+      <button className="bg-teal-500 hover:bg-teal-700 mx-6 rounded shadow text-white mt-4" onClick={async () => {
+        const recentWinnerFromCall = (await getRecentWinner())?.toString();
+        setRecentWinner(recentWinnerFromCall);
+      }}>Recent Winner</button>
+      <span className="text-slate-500 font-semibold">The most recent winner of the lottery is {recentWinner}</span>
+    </div>
+    </div>
+    </div>
     </>
   )
 } 
